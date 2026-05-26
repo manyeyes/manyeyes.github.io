fe72814c61ad8bd408595ffe6e98a0deed57909d
manyeyes <zx.520.521@126.com>
2026-05-26 20:28:12 +0800

    update files

--- manyspeech/rspress.config.ts
+++ manyspeech/rspress.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   // 顶层配置：所有语言共用
   icon: '/manyspeech/manyspeech-icon.webp',
-  logo: {
+  logo: {  
     light: '/manyspeech/manyspeech-light-logo.webp',
     dark: '/manyspeech/manyspeech-dark-logo.webp',
   },
@@ -21,9 +21,14 @@ export default defineConfig({
       {
         icon: 'github',
         mode: 'link',
-        content: 'https://github.com/manyeyes/manyspeech',
+        content: 'https://github.com/manyeyes/manyspeech',  
       },
     ],
+    // 3. 全局页脚（版权 + Powered by + Sitemap链接）
+    footer: {
+      message:
+        '© 2026 ManySpeech, Powered by Rspress<br/>Site last updated 2026-05-26,',
+    },
   },
   
   // 语言配置
@@ -49,5 +54,24 @@ export default defineConfig({
   multiVersion: {
     default: 'v1.0',        // 默认项目（可省略前缀）
     versions: ['v1.0'], // 所有项目列表
+  },
+  builderConfig: {
+    html: {
+      tags: [
+        {
+          tag: 'script',
+          children: `
+            var _hmt = _hmt || [];
+            (function() {
+              var hm = document.createElement("script");
+              hm.src = "https://hm.baidu.com/hm.js?c5cc9be664702972dd979661253d614b";
+              var s = document.getElementsByTagName("script")[0];
+              s.parentNode.insertBefore(hm, s);
+            })();
+          `,
+          head: true,
+        },
+      ],
+    },
   },
 });
